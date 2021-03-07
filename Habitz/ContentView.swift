ff6e69f1ea3726d54960ce38b1213eeb95c80968
Samuel Hepditch <hepditchsam@gmail.com>
2021-03-07 10:39:07 -0500

    Initial Commit

---
+++ Habitz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Habitz
+//
+//  Created by Sam on 2021-03-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
